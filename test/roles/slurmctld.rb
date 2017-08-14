0f0ebf00e19ac561ddafc683fb778b489d508603
Victor Penso <v.penso@gsi.de>
2017-08-14 10:54:44 +0200

    test role to install/configure slurmctld

---
+++ test/roles/slurmctld.rb
@@ -0,0 +1,96 @@
+name 'slurmctld'
+description 'Slurm Cluster Controller deployment'
+run_list( 'recipe[base]' )
+default_attributes(
+ 
+
+  ##
+  # USERS
+  #
+  user: {
+    spock: { 
+      uid: 1111,
+      home: '/network/spock',
+      shell: '/bin/bash'
+    },
+    sulu: { 
+      uid: 1112,
+      home: '/network/sulu',
+      shell: '/bin/bash'
+    },
+    kirk: { 
+      uid: 1113,
+      home: '/network/kirk',
+      shell: '/bin/bash'
+    },
+    uhura: { 
+      uid: 1114,
+      home: '/network/uhura',
+      shell: '/bin/bash'
+    }
+  },
+
+  ##
+  # DIRECTORIES
+  #
+  directory: {
+    ###
+    # Create directories used for NFS export
+    #
+    '/etc/slurm': {},
+    '/network': {},
+    ##
+    # User directories on shared storage
+    #
+    '/network/spock' => { owner: 'spock' },
+    '/network/sulu' => { owner: 'sulu' },
+    '/network/kirk' => { owner: 'kirk' },
+    '/network/uhura' => { owner: 'uhura' }
+  },
+
+  ##
+  # CONFIGURATION FILES
+  #
+  file: {
+     ##
+     # Site specific package repository
+     #
+     '/etc/yum.repos.d/site-local.repo': {
+       content: '
+         [site-local]
+         name=site-local
+         baseurl=http://lxrepo01.devops.test/repo
+         enabled=1
+         gpgcheck=0
+       '
+     },
+     ##
+     # NFS shares configuration
+     #
+     '/etc/exports' => { 
+        content: "/etc/slurm lx*(ro,sync,no_subtree_check)\n/network lx*(rw)\n",
+        notifies: [ :restart, 'systemd_unit[nfs-server.service]' ]
+     },
+     ##
+     # Slurm shared secret
+     #
+     '/etc/munge/munge.key' => {
+        content: '030340d651edb16efabf24a8c080d4b7',
+        notifies: [ :restart, 'systemd_unit[munge.service]' ]
+     }
+  },
+
+  ##
+  # PACKAGES
+  # 
+  package: [ 'nfs-utils', 'slurm' ],
+
+  ##
+  # SYSTEM SERVICES
+  #
+  systemd_unit: {
+    'nfs-server.service': { action: [:enable,:start] },
+    'munge.service': { action: [:enable,:start] },
+    'slurmctld.service': { action: [:enable] }
+  }
+)
