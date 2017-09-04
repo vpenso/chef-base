02f78c95272827f3725fa14940a7e6af3cf9736b
Victor Penso <v.penso@gsi.de>
2017-09-04 10:26:05 +0200

    Better APT example

--- test/roles/apt.rb
+++ test/roles/apt.rb
@@ -2,43 +2,29 @@ name 'apt'
 description 'APT configuration'
 run_list( 'recipe[base]' )
 default_attributes(
-
    file: {
-
      ##
      # APT configuration
      #
-
      '/etc/apt/apt.conf.d/50diff': {
        content: 'Acquire::PDiffs "false";'
      },
-     
      '/etc/apt/apt.conf.d/90recommends': {
        content: 'APT::Install-Recommends 0;'
      },
-
-     '/etc/apt/preferences.d/unstable': {
-       content: %(
-         Pin: release o=Debian,a=unstable
-         Pin-Priority: 400
-       )
-     },
-   
    },
-
+   apt_update: {
+     new_repo: { action: [ :nothing ] }
+   },
+   ##
+   # Additional Repositories
+   #
    apt_repository: {
-   
-     backports: {
+     debian_backports: {
        uri: 'http://ftp.debian.org/debian',
-       distribution: 'jessie-backports',
-       components: [ 'main' ]
-     },
-
-     unstable: {
-       uri: 'http://ftp.de.debian.org/debian',
-       distribution: 'unstable',
-       components: [ 'main' ]
+       distribution: 'stretch-backports',
+       components: [ 'main' ],
+       notifies: [ :update, 'apt_update[new_repo]']
      }
-   
    }
 )
